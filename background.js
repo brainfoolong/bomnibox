e2ebb89ad066376314fdf643cd28462e71c8aa52
ScorpionIbm <blackscorpion.ibm@gmail.com>
2017-04-18 18:13:32 +0200

    Update background.js
    Add the get storage command to the start of search so it always gets the settings when searching

--- background.js
+++ background.js
@@ -17,6 +17,10 @@ var unescapeUrl = function(url){
 
 // on do search - when user typed in the omnibox
 var onSearch = function(text, suggest){
+    chrome.storage.sync.get(["optionTitleSearch"], function(data){
+        optionTitleSearch = false;
+        if(data && data.optionTitleSearch) optionTitleSearch = true;
+    });
     var found = [];
     var s = text.trim().split(" ");
     var sRegex = s;
